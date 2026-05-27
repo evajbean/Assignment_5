b69506a857e0be2e8d6de44f0a36ae99ce9912fa
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-05-27 11:40:18 -0700

    editing parameter defaults and adding output details

--- R/full_program_breakdown.R
+++ R/full_program_breakdown.R
@@ -8,8 +8,8 @@
 #' 
 #' @param max_density maximum number of lobsters that can fit in on square km area
 #' @param K carrying capacity (number of lobsters)
-#' @param r maximum growth rate 
-#' @param m migration rate
-#' @param q catch rate
+#' @param r maximum growth rate; default = 0.3
+#' @param m migration rate; default = 0.1
+#' @param q catch rate; default = 0.5
 #' @param unit_price price paid on the market for one lobster caught ($/lobster)
 #' @param unit_cost cost of establishing an MPA for every unit area ($/km squared)
