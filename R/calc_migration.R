1a36532f4dfe21549b1e55d3be21549e9f37b3c5
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-05-27 11:59:27 -0700

    wrote function to calculate migration rate

---
+++ R/calc_migration.R
@@ -0,0 +1,15 @@
+#' Calculate Migration Rate
+#' 
+#' This function uses the new population size (n_t_in) to calculate the migrated
+#' population outside the MPA (n_t_out)
+#' 
+#' @param n_t_in number of lobsters inside the MPA at time t
+#' @param m migration rate; default = 0.1
+#' output: n_t_out
+
+calc_migration <- function(n_t_in, m){
+  
+  n_t_out = n_t_in*m
+  
+  return(n_t_out)
+}
