66fa62861a4c73f14c12da24a4ea01d3f341c2cf
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-05-27 11:40:47 -0700

    recommit; defaul param editing and output designation

--- R/full_program_breakdown.R
+++ R/full_program_breakdown.R
@@ -1,11 +1,11 @@
 #' Full Program: Calculating Net Profit in Lobster Fisheries Adjacent to MPAs,
 #' depending on MPA Size
-#' Initial inputs for full program: MPA Area, N0 (initial population)
+#' 
 #' 
 #' This (simplified) program calculates the net profit incurred by lobster 
 #' fisheries due to MPA spillover
 #' 
-#' 
+#' Initial inputs for full program: MPA Area, N0 (initial population)
 #' @param max_density maximum number of lobsters that can fit in on square km area
 #' @param K carrying capacity (number of lobsters)
 #' @param r maximum growth rate; default = 0.3
@@ -13,3 +13,4 @@
 #' @param q catch rate; default = 0.5
 #' @param unit_price price paid on the market for one lobster caught ($/lobster)
 #' @param unit_cost cost of establishing an MPA for every unit area ($/km squared)
+#' Outputs: net profit
