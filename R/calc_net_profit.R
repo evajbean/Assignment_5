b54ccae7c70e29754d2f105a8490504caefaf9fc
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-05-29 14:40:42 -0700

    combine calc_profit and net_profit functions

--- R/calc_net_profit.R
+++ R/calc_net_profit.R
@@ -1,18 +1,21 @@
-#' Calculate Catch Profits ($/lobster)
+#' Calculate Net Profits ($)
 #'
 #' This function uses the number of lobsters caught that was calculated in the 
 #' calc_catch.R to calculate total catch profits.
 #'
 #' @param catch number of lobsters caught outside the MPA
 #' @param unit_price price paid on the market for one lobster caught ($/lobster)
+#' @param catch_profit total profit from catch and unit price for lobster overflow ($)
+#' @param net_profit net profit from catch profit, accounting for cost of MPA ($)
 #' 
 #' Put unit_price = 25 as a placeholder
 #' 
-#' output: profit 
+#' @returns net profit  
 
 calc_profit <- function(catch, unit_price=25){
   
-  profit <- catch*unit_price
+  catch_profit <- catch*unit_price
+  net_profit <- cost-catch_profit
   
-  return(profit)
+  return(net_profit)
 }
