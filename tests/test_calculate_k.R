21898f8dc1e0ed0d52d7d933c067fa49582ad078
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-06-02 15:43:47 -0700

    testing calc_k

--- tests/test_calculate_k.R
+++ tests/test_calculate_k.R
@@ -11,8 +11,10 @@ source(here("R/calculate_k.R"))
 # mpa_area should be greater than 0
 # max density parameter cannot be less than 0
 
+test.data <- data.frame(mpa_area=0, max_density=-999)
+
 test_that("calculate_k_works", {
   expect_equal(calculate_k(mpa_area=0), 0)
-  expect_error(calculate_k(mpa_area=-999), "Error: Cost per area of MPA designation cannot be negative")
+  expect_error(calculate_k(test.data), "Error: Cost per area of MPA designation cannot be negative")
 })
   
