b7446ec23a13c32de026e8a354d2e504410ad598
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-06-02 15:44:29 -0700

    testing calculate_k

--- tests/test_calculate_k.R
+++ tests/test_calculate_k.R
@@ -9,12 +9,15 @@ library(janitor)
 source(here("R/calculate_k.R"))
 
 # mpa_area should be greater than 0
-# max density parameter cannot be less than 0
-
-test.data <- data.frame(mpa_area=0, max_density=-999)
+# k cannot be less than 0
 
 test_that("calculate_k_works", {
-  expect_equal(calculate_k(mpa_area=0), 0)
-  expect_error(calculate_k(test.data), "Error: Cost per area of MPA designation cannot be negative")
+
+  test.data <- c(0,10,-999)
+
+  expect_that((calculate_k(mpa_area=test.data[1])), equals(0))
+  small <- calculate_k(mpa_area=test.data[1])
+  big <- calculate_k(mpa_area=test.data[2])
+  expect_true(big>small)
 })
   
