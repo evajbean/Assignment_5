3190f2244d9e6567e4ae43c41098d0e5df1a2385
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-06-02 15:03:27 -0700

    starting test functions

---
+++ tests/test_calculate_k.R
@@ -0,0 +1,18 @@
+#' Test for calculate_k
+
+library(testthat)
+library(tidyverse)
+library(here)
+library(janitor)
+
+# source function
+source(here("R/calculate_k.R"))
+
+# mpa_area should be greater than 0
+# max density parameter cannot be less than 0
+
+test_that("calculate_k_works", {
+  expect_equal(calculate_k(mpa_area=0), 0)
+  expect_error(calculate_k(mpa_area=-999), "Error: Cost per area of MPA designation cannot be negative")
+})
+  
